e48a03b0a97a18f37e8d21dd1c2879b70a19b206
Simon Oldham <simon.oldham@madetech.com>
2022-03-24 10:38:41 +0000

    Backing out inadvertent changes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
-// import pnldDownload from "./pnld-download"
-// import cjsDownload from "./cjs-download"
+import pnldDownload from "./pnld-download"
+import cjsDownload from "./cjs-download"
 import pncDownload from "./pnc-download"
-// import requestedChangesDownload from "./requested-changes-download"
+import requestedChangesDownload from "./requested-changes-download"
 
 const main = async () => {
-  // await pnldDownload()
-  // await cjsDownload()
+  await pnldDownload()
+  await cjsDownload()
   await pncDownload()
-  // await requestedChangesDownload()
+  await requestedChangesDownload()
 }
 
 main()
