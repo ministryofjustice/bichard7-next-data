e73aa7bf6de69486dde6ee1ff56027f2764e3d94
Ben Pirt <ben@pirt.co.uk>
2022-03-28 17:20:33 +0100

    Add github workflow for updating the standing data

--- src/cjs-download/getDownloadUrl.ts
+++ src/cjs-download/getDownloadUrl.ts
@@ -1,18 +1,20 @@
 import axios from "axios"
 
-const dataStandardsUrl = "https://www.gov.uk/guidance/criminal-justice-system-data-standards-forum-guidance"
-const downloadLinkRegex = /https:\/\/.*cjs-offence-index.*ods/
+const dataStandardsUrl =
+  "https://www.gov.uk/guidance/criminal-justice-system-data-standards-forum-guidance"
+const downloadLinkRegex = /(https:\/\/.*cjs.*offence.*index.*ods)"/i
 
-export default (): Promise<string> => axios({
-  url: dataStandardsUrl,
-  method: "GET",
-  responseType: "text"
-})
-  .then((response) => response.data)
-  .then((xhtml) => {
-    const regexResult = downloadLinkRegex.exec(xhtml)
-    if (regexResult && regexResult[0]) {
-      return regexResult[0]
-    }
-    throw new Error("Could not find download URL")
+export default (): Promise<string> =>
+  axios({
+    url: dataStandardsUrl,
+    method: "GET",
+    responseType: "text"
   })
+    .then((response) => response.data)
+    .then((xhtml) => {
+      const regexResult = downloadLinkRegex.exec(xhtml)
+      if (regexResult) {
+        return regexResult[1]
+      }
+      throw new Error("Could not find download URL")
+    })
