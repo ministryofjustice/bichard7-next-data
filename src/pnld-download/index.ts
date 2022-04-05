8d08e4e386f6d98ea08569cdc7efc07254aa5439
Ben Pirt <ben@pirt.co.uk>
2022-04-05 09:53:46 +0100

    Add some more logging for visibility and retry the PNLD download

--- src/pnld-download/index.ts
+++ src/pnld-download/index.ts
@@ -1,13 +1,27 @@
+/* eslint-disable no-await-in-loop, no-console */
 import * as fs from "fs"
 import consistentSort from "../lib/consistentSort"
+import config from "./config"
 import PnldFileDownloader from "./PnldFileDownloader"
 import processPnldFiles from "./processPnldFiles"
-import config from "./config"
 
 export default async () => {
-  const downloader = new PnldFileDownloader(config)
-  const files = await downloader.download()
-  const records = await processPnldFiles(files)
-  const data = consistentSort(records)
-  await fs.promises.writeFile("input-data/offence-code/pnld-offences.json", JSON.stringify(data, null, 2))
+  for (let i = 0; i < 5; i++) {
+    try {
+      console.log("Downloading PNLD data")
+      const downloader = new PnldFileDownloader(config)
+      const files = await downloader.download()
+      const records = await processPnldFiles(files)
+      const data = consistentSort(records)
+      await fs.promises.writeFile(
+        "input-data/offence-code/pnld-offences.json",
+        JSON.stringify(data, null, 2)
+      )
+      console.log("PNLD data successfully downloaded")
+      return
+    } catch (e) {
+      console.error("Error downloading PNLD data")
+      console.error(e)
+    }
+  }
 }
