acabd4e0321210cef8b411c366ca6020a666fe11
csabagyorfi <csaba@madetech.com>
2022-05-03 11:22:46 +0100

    Use explicit false values in valueToBoolean function

--- src/lib/valueToBoolean.ts
+++ src/lib/valueToBoolean.ts
@@ -1,6 +1,15 @@
 export default (value?: string | boolean | null): boolean | undefined => {
-  if (value === undefined || value === null || value === "null") {
+  if (value === undefined || value === null) {
     return undefined
   }
-  return ["Y", "y", true, "Rec"].includes(value)
+
+  const trimmedValue = typeof value === "string" ? value.trim().toLowerCase() : value
+
+  if (["y", true, "rec", "yes"].includes(trimmedValue)) {
+    return true
+  }
+  if (["n", "no", false, "nr"].includes(trimmedValue)) {
+    return false
+  }
+  return undefined
 }
