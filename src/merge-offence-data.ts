cc72650d47d3236c0680a285c9f1f7518402f8ba
csabagyorfi <csaba@madetech.com>
2022-05-09 15:08:31 +0100

    Simplify merge-offence-data
    Update all input data to use boolean types on notifiableToHo and recordableOnPnc fields

--- src/merge-offence-data.ts
+++ src/merge-offence-data.ts
@@ -3,16 +3,16 @@
 
 import fs from "fs"
 import offenceCodeB7CategoryOverrides from "../input-data/offence-code/b7-overrides.json"
-import civilLibraOffenceCodesData from "../input-data/offence-code/civil-libra-codes.json"
-import cjsOffenceCodesData from "../input-data/offence-code/cjs-offences.json"
-import legacyOverridesData from "../input-data/offence-code/legacy-dataset-overrides.json"
-import nrcOffenceCodesData from "../input-data/offence-code/legacy-nrc.json"
-import localOffenceCodesData from "../input-data/offence-code/local-offences.json"
+import civilLibraOffenceCodes from "../input-data/offence-code/civil-libra-codes.json"
+import cjsOffenceCodes from "../input-data/offence-code/cjs-offences.json"
+import legacyOverrides from "../input-data/offence-code/legacy-dataset-overrides.json"
+import nrcOffenceCodes from "../input-data/offence-code/legacy-nrc.json"
+import localOffenceCodes from "../input-data/offence-code/local-offences.json"
 import pncAcpoOffenceCodes from "../input-data/offence-code/pnc-acpo-cjs-offences.json"
 import pncCcjsOffenceCodes from "../input-data/offence-code/pnc-ccjs-cjs-offences.json"
-import pnldOffenceCodesData from "../input-data/offence-code/pnld-offences.json"
+import pnldOffenceCodes from "../input-data/offence-code/pnld-offences.json"
 import unsupportedOffenceCodes from "../input-data/offence-code/unsupported-codes.json"
-import currentOffenceCodesData from "../output-data/data/offence-code.json"
+import currentOffenceCodes from "../output-data/data/offence-code.json"
 import consistentSort from "./lib/consistentSort"
 import HomeOfficeClassifictionPriority from "./merge/HomeOfficeClassificationPriority"
 import NotifiableToHOPriority from "./merge/NotifiableToHOPriority"
@@ -20,26 +20,9 @@ import OffenceCategoryPriority from "./merge/OffenceCategoryPriority"
 import OffenceCodeMerger from "./merge/OffenceCodeMerger"
 import OffenceTitlePriority from "./merge/OffenceTitlePriority"
 import RecordableOnPncPriority from "./merge/RecordableOnPncPriority"
-import mapOffenceCodeData from "./map-data/mapOffenceCodeData"
 import { OffenceCode } from "./types/OffenceCode"
 
-const pncOffenceCodes: OffenceCode[] = mapOffenceCodeData(
-  pncAcpoOffenceCodes.concat(pncCcjsOffenceCodes)
-)
-
-const civilLibraOffenceCodes: OffenceCode[] = mapOffenceCodeData(
-  civilLibraOffenceCodesData as OffenceCode[]
-)
-
-const currentOffenceCodes: OffenceCode[] = mapOffenceCodeData(
-  currentOffenceCodesData as OffenceCode[]
-)
-
-const nrcOffenceCodes: OffenceCode[] = mapOffenceCodeData(nrcOffenceCodesData as OffenceCode[])
-const localOffenceCodes: OffenceCode[] = mapOffenceCodeData(localOffenceCodesData as OffenceCode[])
-const pnldOffenceCodes: OffenceCode[] = mapOffenceCodeData(pnldOffenceCodesData as OffenceCode[])
-const cjsOffenceCodes: OffenceCode[] = mapOffenceCodeData(cjsOffenceCodesData as OffenceCode[])
-const legacyOverrides: OffenceCode[] = mapOffenceCodeData(legacyOverridesData as OffenceCode[])
+const pncOffenceCodes: OffenceCode[] = pncAcpoOffenceCodes.concat(pncCcjsOffenceCodes)
 
 const main = async () => {
   const hoClassification = new HomeOfficeClassifictionPriority(
