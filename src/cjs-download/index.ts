f7e39d6e8dfd2e8c8467f4905fe26058a1f0dfe2
csabagyorfi <csaba@madetech.com>
2022-05-10 10:18:20 +0100

    Remove unnecessary casting

--- src/cjs-download/index.ts
+++ src/cjs-download/index.ts
@@ -4,27 +4,13 @@ import consistentSort from "../lib/consistentSort"
 import convertOds from "./convertOds"
 import downloadCjsFile from "./downloadCjsFile"
 import getDownloadUrl from "./getDownloadUrl"
-import mapOffenceCodeData from "../map-data/mapOffenceCodeData"
-
-type OffenceCodeRecords = {
-  cjsCode: string
-  description?: string
-  homeOfficeClassification?: string | null
-  notifiableToHo?: string
-  recordCreated?: number[]
-  source?: string
-  offenceCategory?: string
-  offenceTitle?: string
-  recordableOnPnc?: string
-  resultHalfLifeHours?: string | null
-}
 
 export default async () => {
   console.log("Downloading CJS data")
   const downloadLocation = await getDownloadUrl()
   const fileContents = await downloadCjsFile(downloadLocation)
   const offenceCodes = convertOds(fileContents)
-  const data = mapOffenceCodeData(consistentSort(offenceCodes) as OffenceCodeRecords[])
+  const data = consistentSort(offenceCodes)
   await fs.promises.writeFile(
     "input-data/offence-code/cjs-offences.json",
     JSON.stringify(data, null, 2)
