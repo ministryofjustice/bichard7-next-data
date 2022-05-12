1fc7ca2cf0fd3ab1684318c048188867428ebac7
csabagyorfi <csaba@madetech.com>
2022-05-12 15:42:33 +0100

    Allow downloading organisation unit data

--- src/organisation-unit-download/index.ts
+++ src/organisation-unit-download/index.ts
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+import * as fs from "fs"
+import consistentSort from "../lib/consistentSort"
 import downloadFile from "../shared/downloadFile"
 import generateOrganisationUnitJSON from "./generateOrganisationUnitObjects"
 
@@ -8,5 +10,10 @@ export default async () => {
     "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1030752/cjs-courts-bc-ou-codes-v32.xls.xlsx"
   const fileContents = await downloadFile(downloadURL)
   const jsonFile = generateOrganisationUnitJSON(fileContents)
-  console.log(jsonFile)
+  const data = consistentSort(jsonFile)
+  await fs.promises.writeFile(
+    "output-data/data/organisation-unit.json",
+    JSON.stringify(data, null, 2)
+  )
+  console.log("Organisation Unit data successfully downloaded")
 }
