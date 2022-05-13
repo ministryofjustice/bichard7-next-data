bccc7113b0cdfbf4f63671674e56ad6f481b0bf6
Joe Folkard <65510707+joe-fol@users.noreply.github.com>
2022-05-13 12:43:27 +0100

    WIP - filter organistion by dates

---
+++ src/organisation-unit-download/valueToDate.ts
@@ -0,0 +1,16 @@
+const valueToDate = (value: any): Date | undefined => {
+  let result: Date | undefined
+
+  if (typeof value === "string") {
+    result = new Date(value)
+  } else if (typeof value === "number") {
+    result = new Date((value - (25567 + 2)) * 86400 * 1000)
+  }
+
+  if (result instanceof Date && Number.isNaN(result.getTime())) {
+    return undefined
+  }
+  return result
+}
+
+export default valueToDate
