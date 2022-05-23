3a2b49f71528db3494d59b577c96f94b4105a011
csabagyorfi <csaba@madetech.com>
2022-05-23 21:56:47 +0100

    Backfill thirdLevelPsaCode on Organisation Unit data
    Backfill this from the existing data when downloading new data

--- src/organisation-unit-download/index.ts
+++ src/organisation-unit-download/index.ts
@@ -1,17 +1,25 @@
 /* eslint-disable no-console */
+/* eslint-disable import/no-relative-packages */
 import * as fs from "fs"
+import existingOrganisationUnitData from "../../output-data/data/organisation-unit.json"
 import getDownloadUrl from "../cjs-download/getDownloadUrl"
 import consistentSort from "../lib/consistentSort"
 import downloadFile from "../lib/downloadFile"
-import generateOrganisationUnitJSON from "./generateOrganisationUnitObjects"
+import generateOrganisationUnitObjects from "./generateOrganisationUnitObjects"
+import backFillThirdLevelPsaCode from "./backFillThirdLevelPsaCode"
+import { OrganisationUnit } from "../types/OrganisationUnit"
 
 export default async () => {
   console.log("Downloading Organisation Unit data")
   const downloadLinkRegex = /(https:\/\/.*cjs-courts-bc-ou-codes.*.xls.xlsx)"/i
   const downloadURL = await getDownloadUrl(downloadLinkRegex)
   const fileContents = await downloadFile(downloadURL)
-  const jsonFile = generateOrganisationUnitJSON(fileContents)
-  const data = consistentSort(jsonFile)
+  const newOrganisationUnitData = generateOrganisationUnitObjects(fileContents)
+  const jsonFileContent = backFillThirdLevelPsaCode(
+    newOrganisationUnitData,
+    existingOrganisationUnitData as OrganisationUnit[]
+  )
+  const data = consistentSort(jsonFileContent)
   await fs.promises.writeFile(
     "output-data/data/organisation-unit.json",
     JSON.stringify(data, null, 2)
