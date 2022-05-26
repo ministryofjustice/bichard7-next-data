1d1e04fc3b368b2972451a944fde16ecbc3b6371
csabagyorfi <csaba@madetech.com>
2022-05-26 16:06:16 +0100

    Update organisation unit download script to add police data

--- src/organisation-unit-download/index.ts
+++ src/organisation-unit-download/index.ts
@@ -5,21 +5,25 @@ import existingOrganisationUnitData from "../../output-data/data/organisation-un
 import getDownloadUrl from "../cjs-download/getDownloadUrl"
 import consistentSort from "../lib/consistentSort"
 import downloadFile from "../lib/downloadFile"
-import generateOrganisationUnitObjects from "./generateOrganisationUnitObjects"
+import generateCourtOrganisationUnits from "./generateCourtOrganisationUnits"
 import backFillThirdLevelPsaCode from "./backFillThirdLevelPsaCode"
 import { OrganisationUnit } from "../types/OrganisationUnit"
+import policeOrganisationUnitData from "../../input-data/organisation-unit/police-forces.json"
 
 export default async () => {
   console.log("Downloading Organisation Unit data")
   const downloadLinkRegex = /(https:\/\/.*cjs-courts-bc-ou-codes.*.xls.xlsx)"/i
   const downloadURL = await getDownloadUrl(downloadLinkRegex)
   const fileContents = await downloadFile(downloadURL)
-  const newOrganisationUnitData = generateOrganisationUnitObjects(fileContents)
-  const jsonFileContent = backFillThirdLevelPsaCode(
-    newOrganisationUnitData,
+  const courtOrganisationUnitData = generateCourtOrganisationUnits(fileContents)
+  const courtOrganisationUnitDataWithPsaCode = backFillThirdLevelPsaCode(
+    courtOrganisationUnitData,
     existingOrganisationUnitData as OrganisationUnit[]
   )
-  const data = consistentSort(jsonFileContent)
+  const allOrganisationUnitData = courtOrganisationUnitDataWithPsaCode.concat(
+    policeOrganisationUnitData
+  )
+  const data = consistentSort(allOrganisationUnitData)
   await fs.promises.writeFile(
     "output-data/data/organisation-unit.json",
     JSON.stringify(data, null, 2)
