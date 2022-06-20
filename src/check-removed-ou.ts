eec00e2d44ecc604146e0c9231b60ee760690fb8
csabagyorfi <csaba@madetech.com>
2022-06-20 10:48:32 +0100

    WIP

--- src/check-removed-ou.ts
+++ src/check-removed-ou.ts
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-relative-packages */
 import fs from "fs"
 import newOrganisationUnitData from "../output-data/data/organisation-unit.json"
-import existingOrganisationUnitData from "../output-data/data/organisation-unit-legacy.json"
+import legacyOrganisationUnitData from "../output-data/data/organisation-unit-legacy.json"
 import { OrganisationUnit } from "./types/OrganisationUnit"
 
 const matchingValues = (firstValue: string, secondValue: string): boolean =>
@@ -10,7 +10,7 @@ const matchingValues = (firstValue: string, secondValue: string): boolean =>
 
 const checkForRemoved = (newData: OrganisationUnit[], oldData: OrganisationUnit[]): string => {
   const matchNotFoundForRecords: OrganisationUnit[] = []
-  let changes: string = ""
+  let changes: string[] = []
   let numberOfChanges = 0
 
   oldData.forEach((oldRecord) => {
@@ -23,47 +23,46 @@ const checkForRemoved = (newData: OrganisationUnit[], oldData: OrganisationUnit[
     )
     if (match) {
       let hasDiff = false
-      let thisChange = "\n__________________\n"
+      const thisChange = []
 
-      thisChange = thisChange.concat(`topLevelCode: ${oldRecord.topLevelCode}\n`)
-      thisChange = thisChange.concat(`secondLevelCode: ${oldRecord.secondLevelCode}\n`)
-      thisChange = thisChange.concat(`thirdLevelCode: ${oldRecord.thirdLevelCode}\n`)
-      thisChange = thisChange.concat(`bottomLevelCode: ${oldRecord.bottomLevelCode}\n`)
+      // thisChange = thisChange.concat(`topLevelCode: ${oldRecord.topLevelCode}\n`)
+      // thisChange = thisChange.concat(`secondLevelCode: ${oldRecord.secondLevelCode}\n`)
+      // thisChange = thisChange.concat(`thirdLevelCode: ${oldRecord.thirdLevelCode}\n`)
+      // thisChange = thisChange.concat(`bottomLevelCode: ${oldRecord.bottomLevelCode}\n`)
 
-      // if(oldRecord.topLevelName !== newData[i].topLevelName) {
-      //   hasDiff = true
-      //   thisChange = thisChange.concat("topLevelName: " + oldRecord.topLevelName + " -> " + newData[i].topLevelName + "\n")
-      // } else {
-      //   thisChange = thisChange.concat("topLevelName: " + oldRecord.topLevelName + "\n")
-      // }
-      // if(oldRecord.secondLevelName !== newData[i].secondLevelName) {
-      //   hasDiff = true
-      //   thisChange = thisChange.concat("secondLevelName: " + oldRecord.secondLevelName + " -> " + newData[i].secondLevelName + "\n")
-      // } else {
-      //   thisChange = thisChange.concat("secondLevelName: " + oldRecord.secondLevelName + "\n")
-      // }
-      // if(oldRecord.thirdLevelName !== newData[i].thirdLevelName) {
-      //   hasDiff = true
-      //   thisChange = thisChange.concat("thirdLevelName: " + oldRecord.thirdLevelName + " -> " + newData[i].thirdLevelName + "\n")
-      // } else {
-      //   thisChange = thisChange.concat("thirdLevelName: " + oldRecord.thirdLevelName + "\n")
-      // }
-      // if(oldRecord.bottomLevelName !== newData[i].bottomLevelName) {
-      //   hasDiff = true
-      //   thisChange = thisChange.concat("bottomLevelName: " + oldRecord.bottomLevelName + " -> " + newData[i].bottomLevelName + "\n")
-      // } else {
-      //   thisChange = thisChange.concat("bottomLevelName: " + oldRecord.bottomLevelName + "\n")
-      // }
+      if (oldRecord.topLevelName !== match.topLevelName) {
+        hasDiff = true
+        thisChange.push(`topLevelName: ${oldRecord.topLevelName} -> ${match.topLevelName}`)
+      } else {
+        // thisChange = thisChange.concat("topLevelName: " + oldRecord.topLevelName + "\n")
+      }
+      if (oldRecord.secondLevelName !== match.secondLevelName) {
+        hasDiff = true
+        thisChange.push(`secondLevelName: ${oldRecord.secondLevelName} -> ${match.secondLevelName}`)
+      } else {
+        // thisChange = thisChange.concat("secondLevelName: " + oldRecord.secondLevelName + "\n")
+      }
+      if (oldRecord.thirdLevelName !== match.thirdLevelName) {
+        hasDiff = true
+        thisChange.push(`thirdLevelName: ${oldRecord.thirdLevelName} -> ${match.thirdLevelName}\n`)
+      } else {
+        // thisChange = thisChange.concat("thirdLevelName: " + oldRecord.thirdLevelName + "\n")
+      }
+      if (oldRecord.bottomLevelName !== match.bottomLevelName) {
+        hasDiff = true
+        thisChange.push(`bottomLevelName: ${oldRecord.bottomLevelName} -> ${match.bottomLevelName}`)
+      } else {
+        // thisChange = thisChange.concat("bottomLevelName: " + oldRecord.bottomLevelName + "\n")
+      }
 
       if (oldRecord.thirdLevelPsaCode !== match.thirdLevelPsaCode) {
         hasDiff = true
-        thisChange = thisChange.concat(
-          `thirdLevelPsaCode: ${oldRecord.thirdLevelPsaCode} -> ${match.thirdLevelPsaCode}\n`
+        thisChange.push(
+          `thirdLevelPsaCode: ${oldRecord.thirdLevelPsaCode} -> ${match.thirdLevelPsaCode}`
         )
       } else {
-        thisChange = thisChange.concat(`thirdLevelPsaCode: ${oldRecord.thirdLevelPsaCode}\n`)
+        // thisChange = thisChange.concat(`thirdLevelPsaCode: ${oldRecord.thirdLevelPsaCode}\n`)
       }
-      thisChange = thisChange.concat("\n__________________\n")
       if (hasDiff) {
         numberOfChanges += 1
         changes = changes.concat(thisChange)
@@ -80,12 +79,13 @@ const checkForRemoved = (newData: OrganisationUnit[], oldData: OrganisationUnit[
 
   console.log(`Changed records ${numberOfChanges}`)
 
-  return changes
+  const result = new Set(changes)
+  return JSON.stringify([...result])
 }
 
 const main = async () => {
-  const result = checkForRemoved(newOrganisationUnitData, existingOrganisationUnitData)
-  await fs.promises.writeFile("./diffs", result)
+  const result = checkForRemoved(newOrganisationUnitData, legacyOrganisationUnitData)
+  await fs.promises.writeFile("./diffs.json", result)
 }
 
 main()
