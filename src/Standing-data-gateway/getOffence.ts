fa4525cb346abf2ec10322c8ffe780ac946af6b9
Donny <donny.hyon@madetech.com>
2023-03-17 12:17:39 +0000

    Added todo list
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

--- src/Standing-data-gateway/getOffence.ts
+++ src/Standing-data-gateway/getOffence.ts
@@ -5,6 +5,12 @@ import { offenceBody, devApiUrl } from "./apiConfig"
 
 const fileCreatedNotification = () => console.log("file created")
 // TODO- apply zod type to file
+// TODO after discussions with ben:
+// TODO- create a file that replicates the `download-offence-code-data`
+// create a map function that calls over every single letter in the AlphaChar param
+// ensure consistent sort is used so that you can diff between revisions of files
+// create new file in input-data folder to have `standing-data-api.json` file with all offence codes
+
 const getOffence = () => {
   axios
     .post(devApiUrl, offenceBody, {
