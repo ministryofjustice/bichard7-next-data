cdb83a0736f099e60291db1cab20fc4b1c7bc641
Donny <donny.hyon@madetech.com>
2023-03-28 14:17:38 +0100

    Remvoed error from api response
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

--- src/Standing-data-gateway/index.ts
+++ src/Standing-data-gateway/index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs"
+import consistentSort from "../lib/consistentSort"
+import convertApiResponse from "./convertApiResponse"
 import getOffence from "./getOffence"
 
 // const listofChar = [..."ABCDEFGHIJKLMNOPQR"]
@@ -5,8 +8,16 @@ import getOffence from "./getOffence"
 export default async () => {
   console.log("Calling Standing Data API")
   //   get the API response == "fileContents"
-  const fileContents = await getOffence("A")
-  console.log(fileContents)
+  const apiResponse = await getOffence("N")
+  // handle error
+  console.log("Retrieved API response")
+  const offenceCodes = convertApiResponse(apiResponse)
+
+  const data = consistentSort(offenceCodes)
+  await fs.promises.writeFile(
+    "input-data/offence-code/standing-data-offences.json",
+    JSON.stringify(data, null, 2)
+  )
 
   //  covertODS- map API response keys to align with input-data keys. == "offenceCodes"
   // consistent sort
