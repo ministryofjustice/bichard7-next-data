8888e3d3492b3662dde99cb3851fc7ebe261786a
Donny <donny.hyon@madetech.com>
2023-03-29 16:56:47 +0100

    Updated todos
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

--- src/Standing-data-gateway/getOffence.ts
+++ src/Standing-data-gateway/getOffence.ts
@@ -5,9 +5,22 @@ import { ApiOffence } from "../types/StandingDataAPIResult"
 import { getOffenceApiResultSchema } from "../schemas/standingDataAPIResult"
 
 // TODO- go through missing json data.
+// clarify where recordable on PNC data. Look at the merger file.
+//  From invesitgation looking at the `merge-offence-data.ts` cjs-download is not being used to populate recordable on PNC so does it really amtter if its missing from api return? - For Ben.
+
 // TODO -  diff between revisions of files
+//  cat input-data/offence-code/current-standing-data-offences.json | jq '..|.cjsCode?' | grep "[A-Z][A-Z]*" | wc -w
+//  cat input-data/offence-code/current-standing-data-offences.json | jq '..|.cjsCode?' | grep "[A-Z][A-Z]*" | wc -w | uniq
+//  cat input-data/offence-code/all-standing-data-offences.json | jq '..|.cjsCode?' | grep "[A-Z][A-Z]*" | wc -w | uniq
+//  cat input-data/offence-code/all-standing-data-offences.json | jq '..|.cjsCode?' | grep "[A-Z][A-Z]*" | wc -w
 
 // TODO - create new file in input-data folder to have `standing-data-api.json` file with all offence codes
+
+// POTENTIAL NEXT STEPS:
+// FIND OUT script that generates the output-data.json files.
+// Point to standing-data.json rather than cjs-offences.json
+// compare output data from standing vs cjs download.
+// TODO check assumption with Ben that we are replacing `cjs-offence.json` file with standing data. cjs-offence only has 14k codes, whereas standing data returns 23k...
 
 const getOffence = async (alphaChar: string): Promise<ApiOffence[] | Error> => {
   console.log(`running getOffence() with "${alphaChar}"`)
@@ -23,6 +36,7 @@ const getOffence = async (alphaChar: string): Promise<ApiOffence[] | Error> => {
     })
     .then((result) => {
       const parsedApiResult = getOffenceApiResultSchema.parse(result.data)
+      console.log(result.data.MessageBody.GatewayOperationType.GetOffenceResponse.Offence)
       const newResult = parsedApiResult.MessageBody.GatewayOperationType.GetOffenceResponse.Offence
       console.log(`success with ${alphaChar}`)
       return newResult
@@ -32,6 +46,7 @@ const getOffence = async (alphaChar: string): Promise<ApiOffence[] | Error> => {
       return error
     })
 }
+getOffence("A")
 
 // TODO check if zodding inside the axios call is the clean way to do things.
 // TODO - check the best way to call in payload config.
