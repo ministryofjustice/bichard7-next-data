b20c1babb59623f5c586034d63455899b7419324
Donny <donny.hyon@madetech.com>
2023-03-29 12:30:47 +0100

    downloaded all data

--- src/Standing-data-gateway/index.ts
+++ src/Standing-data-gateway/index.ts
@@ -6,15 +6,26 @@ import getOffence from "./getOffence"
 export default async () => {
   console.log("Calling Standing Data API")
 
+  // const subListOfAlphaChar = [..."AB"]
   const subListOfAlphaChar = [..."ABCDEFGHI"]
   const promisedTasks = subListOfAlphaChar.map((char) => getOffence(char))
   const apiResponses = await Promise.all(promisedTasks)
 
-  // handle error
-  const offenceCodes = apiResponses.map((apiResponse) => {
-    return convertApiResponse(apiResponse)
-  })
-  const data = consistentSort(offenceCodes)
+  // const subListOfAlphaChar2 = [..."CD"]
+  const subListOfAlphaChar2 = [..."JKLMNOPR"]
+  const promisedTasks2 = subListOfAlphaChar2.map((char) => getOffence(char))
+  const apiResponses2 = await Promise.all(promisedTasks2)
+
+  // const subListOfAlphaChar3 = [..."EF"]
+  const subListOfAlphaChar3 = [..."STUVWXYZ"]
+  const promisedTasks3 = subListOfAlphaChar3.map((char) => getOffence(char))
+  const apiResponses3 = await Promise.all(promisedTasks3)
+
+  const allApiResponses = [apiResponses, apiResponses2, apiResponses3].flat()
+  console.log(allApiResponses.length)
+
+  const offenceCodes = allApiResponses.map((responses) => convertApiResponse(responses))
+  const data = consistentSort(offenceCodes.flat())
   await fs.promises.appendFile(
     "input-data/offence-code/standing-data-offences.json",
     JSON.stringify(data, null, 2)
