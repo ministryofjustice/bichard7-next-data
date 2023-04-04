8f45ce4236491d647042158cf981d29ebb88f399
Donny <donny.hyon@madetech.com>
2023-04-04 09:19:16 +0100

    Removed console.log
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

--- src/standing-data-gateway/convertApiResponse.ts
+++ src/standing-data-gateway/convertApiResponse.ts
@@ -3,7 +3,6 @@ import { ApiOffence } from "../types/StandingDataAPIResult"
 
 export default (apiResponse: ApiOffence[] | Error): OffenceCode[] => {
   if (apiResponse instanceof Error) {
-    console.log(Error)
     throw new Error("apiResponse resulted in Error output")
   }
   return apiResponse.map((offenceCode) => ({
