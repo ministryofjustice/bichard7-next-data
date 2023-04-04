ed5202bc78eacf8509f87a75508a4d31150eff71
Donny Hyon <105787366+donnyhyon@users.noreply.github.com>
2023-04-04 11:25:04 +0100

    BICAWS-2064 Test Standing Data API (#122)
    * WIP - Standing data
    
    * Leaf signature error fixed
    
    * Pulling data from API
    
    * created a function for geting data
    
    * added httpsAgent config
    
    * WIP
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Mapped out api response to object
    
    * Refactored transform function
    
    * Broken out api config into own file
    
    * console logging all the data
    
    * moved into one function
    
    * WIP
    
    * got output for cjscodes in json file
    
    * updated name for function
    
    * renamed variables
    
    * Updated timestamp logic
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Fixed zod typing
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Renamed filename
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added config for getOffence and getApplication
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added getOffence request
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Renamed file to getMojOffence
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Updated API result schema to handle specific reqs
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Created getApplication request
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Renamed zod typing to be more specific to api call
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added todo list
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * added zod to getOffence request
    
    * Updated offencewording to cjstitle
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * WIP- changing alphachar
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added new uuid() function for each body
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added map function to go through all alphachars
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Plumbed in download command using package json
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Remvoed error from api response
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Fixed pipeline for subset of alphaChar
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * broke out consistent sort
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * downloaded all data
    
    * Removed outdated comments
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Added TODO about non returned "Q" request
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Changed console.logs
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed TODOs
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Updated todos
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * added todo
    
    * Broken API call down into chunks
    
    * removed old comments
    
    * implemented env vars
    
    * Rename folder to align with convention
    
    * Rename folder to align with convention
    
    * Remove getMojOffences
    
    * Added npm command to update-standing-data
    
    * Added npm command to update-standing-data
    
    * Removed comments
    
    * added env variables to GH config
    
    * Updated to writeFile
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed console.log
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed console.log
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed further console.logs
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed console.log
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    * Removed another console.log
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    
    ---------
    
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

---
+++ src/standing-data-gateway/index.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs"
+import consistentSort from "../lib/consistentSort"
+import convertApiResponse from "./convertApiResponse"
+import getOffence from "./getOffence"
+
+export default async () => {
+  console.log("Calling Standing Data API")
+  const alphaChar = [..."ABCDEFGHIJKLMNOPRSTUVWXYZ"] // "Q" Removed as not a valid request
+  const chunks = 5
+  const allApiResponses = []
+  for (let i = 0; i < alphaChar.length; i += chunks) {
+    let tempArray = []
+    tempArray = alphaChar.slice(i, i + chunks)
+    const promisedTasks = tempArray.map(getOffence)
+    // eslint-disable-next-line no-await-in-loop
+    allApiResponses.push(await Promise.all(promisedTasks))
+  }
+
+  const offenceCodes = allApiResponses.flat().map(convertApiResponse)
+  const data = consistentSort(offenceCodes.flat())
+  await fs.promises.writeFile(
+    "input-data/offence-code/standing-data-offences.json",
+    JSON.stringify(data, null, 2)
+  )
+
+  console.log("Standing Data API data successfully retrieved")
+}
