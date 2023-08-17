c776a7fa1e155fe9d7b86b3470a9635d232af86b
Donny <donny.hyon@madetech.com>
2023-08-17 10:45:15 +0100

    removed request changes step

--- src/download-offence-code-data.ts
+++ src/download-offence-code-data.ts
@@ -1,13 +1,11 @@
 import cjsDownload from "./cjs-download"
 // import pncDownload from "./pnc-download"
 import pnldDownload from "./pnld-download"
-import requestedChangesDownload from "./requested-changes-download"
 
 const main = async () => {
   await pnldDownload()
   await cjsDownload()
   // await pncDownload()
-  await requestedChangesDownload()
 }
 
 main().then(() => console.log("All data downloaded"))
