895d7ac3de7d8368c677b7321e0c74586798942e
joe-fol <joefolkard@protonmail.com>
2024-08-20 14:44:00 +0100

    Remove reference to cjsDownload

--- src/download-offence-code-data.ts
+++ src/download-offence-code-data.ts
@@ -1,9 +1,7 @@
-import cjsDownload from "./cjs-download"
 import pnldDownload from "./pnld-download"
 
 const main = async () => {
   await pnldDownload()
-  await cjsDownload()
 }
 
 main().then(() => console.log("All data downloaded"))
