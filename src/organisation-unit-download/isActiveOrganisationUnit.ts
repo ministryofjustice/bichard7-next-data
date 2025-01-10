497f2fd925d5e075a6ac0816c49dfd8c0e449a98
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2025-01-10 11:11:33 +0000

    Fix downloading organisation unit codes

--- src/organisation-unit-download/isActiveOrganisationUnit.ts
+++ src/organisation-unit-download/isActiveOrganisationUnit.ts
@@ -4,14 +4,12 @@ const isActiveOrganisationUnit = (start?: string, end?: string): boolean => {
   const startDate = valueToDate(start)
   const endDate = valueToDate(end)
   const currentDate = new Date()
-  if (
-    startDate &&
+
+  return (
+    !!startDate &&
     startDate.getTime() <= currentDate.getTime() &&
-    (endDate === undefined || endDate.getTime() >= new Date().getTime())
-  ) {
-    return true
-  }
-  return false
+    (endDate === undefined || endDate.getTime() >= currentDate.getTime())
+  )
 }
 
 export default isActiveOrganisationUnit
--- src/organisation-unit-download/valueToDate.ts
+++ src/organisation-unit-download/valueToDate.ts
@@ -1,18 +1,18 @@
-const valueToDate = (value: any): Date | undefined => {
+const valueToDate = (value?: string | number | null): Date | undefined => {
   let result: Date | undefined
 
-  if (value === undefined || value === "" || value === null) {
+  if (value === undefined || value === null || (typeof value === "string" && value.trim()) === "") {
     return undefined
   }
 
   if (typeof value === "string") {
-    result = new Date(value)
-  } else if (typeof value === "number") {
+    result = new Date(value.trim())
+  } else {
     result = new Date((value - (25567 + 2)) * 86400 * 1000)
   }
 
-  if (result instanceof Date && Number.isNaN(result.getTime())) {
-    console.log(`INVALID DATE FORMAT: ${value}`)
+  if (result && Number.isNaN(result.getTime())) {
+    console.log(`INVALID DATE FORMAT: [${value}]`)
     return undefined
   }
   return result
