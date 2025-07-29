651182e425d4629d68b53eb44783dd1b8292f474
Tausif Patel <tausifpatel.dev@gmail.com>
2025-07-29 16:31:51 +0100

    Update mergeCjsResultCodes to overwrite matching entries with new values

--- src/cjs-result-codes-download/mergeCjsResultCodes.test.ts
+++ src/cjs-result-codes-download/mergeCjsResultCodes.test.ts
@@ -6,6 +6,84 @@ describe("MergeCjsResultCodes", () => {
     const newData: ResultCode[] = [
       {
         cjsCode: "1001",
+        description: "Death",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "",
+        resultHalfLifeHours: "72",
+        type: "F"
+      },
+      {
+        cjsCode: "1002",
+        description: "Imprisonment",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AB",
+        resultHalfLifeHours: "72",
+        type: "F"
+      }
+    ]
+
+    const existingData: ResultCode[] = [
+      {
+        cjsCode: "1003",
+        description: "Detained during HM Pleasure",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AJ",
+        resultHalfLifeHours: "72",
+        type: "F"
+      },
+      {
+        cjsCode: "1004",
+        description: "Restriction Order",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AV",
+        resultHalfLifeHours: "72",
+        type: "F"
+      }
+    ]
+
+    const expectedData: ResultCode[] = [
+      {
+        cjsCode: "1003",
+        description: "Detained during HM Pleasure",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AJ",
+        resultHalfLifeHours: "72",
+        type: "F"
+      },
+      {
+        cjsCode: "1004",
+        description: "Restriction Order",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AV",
+        resultHalfLifeHours: "72",
+        type: "F"
+      },
+      {
+        cjsCode: "1001",
+        description: "Death",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "",
+        resultHalfLifeHours: "72",
+        type: "F"
+      },
+      {
+        cjsCode: "1002",
+        description: "Imprisonment",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "AB",
+        resultHalfLifeHours: "72",
+        type: "F"
+      }
+    ]
+
+    const mergedData = mergeCjsResultCodes(newData, existingData)
+    expect(mergedData).toEqual(expectedData)
+  })
+
+  it("Does not create duplicate entries", () => {
+    const newData: ResultCode[] = [
+      {
+        cjsCode: "1001",
         description: "Uncoded text result",
         resultCodeQualifiers: "",
         resultHalfLifeHours: "",
@@ -63,6 +141,44 @@ describe("MergeCjsResultCodes", () => {
         resultCodeQualifiers: "",
         resultHalfLifeHours: "",
         type: "F"
+      }
+    ]
+
+    const mergedData = mergeCjsResultCodes(newData, existingData)
+    expect(mergedData).toEqual(expectedData)
+  })
+
+  it("Overwrites existing result codes with values from new data", () => {
+    const newData: ResultCode[] = [
+      {
+        cjsCode: "1001",
+        description: "Uncoded text result",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "F,FH,YP,YQ,YT,C",
+        resultHalfLifeHours: "72",
+        type: ""
+      }
+    ]
+
+    const existingData: ResultCode[] = [
+      {
+        cjsCode: "1001",
+        description: "Imprisonment",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "",
+        resultHalfLifeHours: "",
+        type: "F"
+      }
+    ]
+
+    const expectedData: ResultCode[] = [
+      {
+        cjsCode: "1001",
+        description: "Uncoded text result",
+        recordableOnPnc: "",
+        resultCodeQualifiers: "F,FH,YP,YQ,YT,C",
+        resultHalfLifeHours: "72",
+        type: ""
       }
     ]
 
--- src/cjs-result-codes-download/mergeCjsResultCodes.ts
+++ src/cjs-result-codes-download/mergeCjsResultCodes.ts
@@ -1,39 +1,20 @@
 import { ResultCode } from "../../output-data/types/types"
 
-const matchingValues = (
-  firstValue: string | undefined,
-  secondValue: string | undefined
-): boolean => {
-  if (firstValue === undefined || secondValue === undefined) {
-    return false
-  }
-
-  return firstValue?.trim().toLowerCase() === secondValue?.trim().toLowerCase()
-}
+const matchingValues = (firstValue: string, secondValue: string): boolean =>
+  firstValue?.trim().toLowerCase() === secondValue?.trim().toLowerCase()
 
 const mergeCjsResultCodes = (newData: ResultCode[], existingData: ResultCode[]): ResultCode[] => {
   newData.forEach((newItem) => {
-    const existingRecord = existingData.find(
-      (existingItem) =>
-        matchingValues(existingItem.cjsCode, newItem.cjsCode) &&
-        matchingValues(existingItem.description, newItem.description) &&
-        matchingValues(existingItem.recordableOnPnc, newItem.recordableOnPnc) &&
-        matchingValues(existingItem.resultCodeQualifiers, newItem.resultCodeQualifiers) &&
-        matchingValues(existingItem.resultHalfLifeHours, newItem.resultHalfLifeHours) &&
-        matchingValues(existingItem.type, newItem.type)
+    const existingRecord = existingData.find((existingItem) =>
+      matchingValues(existingItem.cjsCode, newItem.cjsCode)
     )
 
-    console.log("====>", existingRecord)
-
     if (existingRecord) {
-      existingRecord.cjsCode = newItem.cjsCode || existingRecord.cjsCode
-      existingRecord.description = newItem.description || existingRecord.description
-      existingRecord.recordableOnPnc = newItem.recordableOnPnc || existingRecord.recordableOnPnc
-      existingRecord.resultCodeQualifiers =
-        newItem.resultCodeQualifiers || existingRecord.resultCodeQualifiers
-      existingRecord.resultHalfLifeHours =
-        newItem.resultHalfLifeHours || existingRecord.resultHalfLifeHours
-      existingRecord.type = newItem.type || existingRecord.type
+      existingRecord.description = newItem.description
+      existingRecord.recordableOnPnc = newItem.recordableOnPnc
+      existingRecord.resultCodeQualifiers = newItem.resultCodeQualifiers
+      existingRecord.resultHalfLifeHours = newItem.resultHalfLifeHours
+      existingRecord.type = newItem.type
     } else {
       existingData.push(newItem)
     }
