06bfe2ada2cf2bb07777de8d4e7b120790cbb2b4
joe folkard <joe.folkard@madetech.com>
2026-08-25 09:53:12 +0100

    Pin typescript

--- .ncurc.js
+++ .ncurc.js
@@ -1,7 +1,7 @@
 // google packages are here because upgrading them
 // currently breaks their compatibility
 // affects: src/requested-changes-download/sheetsClient.ts
-const pinned = ["google-auth-library", "eslint", "eslint-plugin-jest", "jest"]
+const pinned = ["google-auth-library", "eslint", "eslint-plugin-jest", "jest", "typescript"]
 const ignored = ["googleapis", "puppeteer"]
 const skipped = []
 
